adbb178fb888959b19ed056223abc0ab9cee4739
Pierre-Hansquine <97633589+Pierre-Hansquine@users.noreply.github.com>
2022-06-15 12:15:40 +0200

    Update streamlit_app.py
    Adjusted button label, added statement to close connection after the button script, and added header.

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,10 +51,12 @@ def get_fruit_load_list():
          my_cur.execute("SELECT * FROM fruit_load_list")
          return my_cur.fetchall()
         
-# Add a button to load the fruit
-if streamlit.button('Get Fruit Load List'):
+streamlit.header("View Our Fruit List - Add Your Favorites!")
+        # Add a button to load the fruit
+if streamlit.button('Get Fruit List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
+    my_cnx.close()
     streamlit.dataframe(my_data_rows)
 
 # Allow the end user to add fruit to the list
